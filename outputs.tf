a7eeb1cae6225003d43a100bb1d4a4c9fe38a065
Cloud42 <hello@cloud42.io>
2020-12-21 11:38:55 +0000

    Add arn as output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "alarm" {
   description = "CloudWatch metric alarm"
   value       = aws_cloudwatch_metric_alarm.this
 }
+
+output "arn" {
+  description = "CloudWatch metric alarm arn"
+  value       = aws_cloudwatch_metric_alarm.this.arn
+}
+
